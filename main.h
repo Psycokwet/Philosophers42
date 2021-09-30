d74262a1210001145c53410f06ac3e6dcb7a22e8
Sophie <socarboni@gmail.com>
2021-09-30 23:50:53 +0200

    add includes

--- main.h
+++ main.h
@@ -16,4 +16,32 @@
 # include <stddef.h>
 # include <stdbool.h>
 
+# include <string.h>
+
+void *memset(void *s, int c, size_t n);
+
+# include <stdio.h>
+// printf
+# include <stdlib.h>
+// malloc
+// free
+# include <unistd.h>
+// ssize_t write(int fd, const void *buf, size_t count);
+# include <unistd.h>
+//int usleep(useconds_t usec);
+# include <sys/time.h>
+//int gettimeofday(struct timeval *tv, struct timezone *tz);
+# include <pthread.h>
+
+//int pthread_create(pthread_t *thread, const pthread_attr_t *attr,
+//			void *(*start_routine) (void *), void *arg);
+//int pthread_detach(pthread_t thread);
+//int pthread_join(pthread_t thread, void **retval);
+// pthread_mutex_init
+// pthread_mutex_destroy
+// pthread_mutex_lock
+// pthread_mutex_unlock
+
+
+
 #endif
